253e3a4cb3f75c84f5b67b1e369ea42c5bff8523
eric karnis <erickarnis@gmail.com>
2021-05-25 20:54:47 -0400

    updated variable names and added video by id

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from tokify import trending
+from tokify import trending, video_by_id
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def get_trending():
     return jsonify(trending())
 
+
+@app.route('/download/<id>')
+def get_video_by_id(id):
+    return video_by_id(id)
 if __name__=='__main__':
     app.run(debug=False)
