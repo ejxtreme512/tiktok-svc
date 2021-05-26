32e3506d061df82d5b85b33172b8f21d027c2bc7
Andrew <andrew.karnis@gmail.com>
2021-05-25 22:51:34 -0400

    update methods

--- tokify.py
+++ tokify.py
@@ -1,11 +1,9 @@
-from flask.json import jsonify
+import random
+import io
+import string
+from TikTokApi import TikTokApi
 from gevent import monkey
 monkey.patch_all()
-
-from TikTokApi import TikTokApi, tiktok
-
-import string
-import random
 
 
 verifyFp = 'verify_kp3dk3o5_vO4w3sVy_2RCe_4SqH_BBuY_VINzWCvoX0GN'
@@ -15,22 +13,30 @@ did = ''.join(random.choice(string.digits) for num in range(10))
 api = TikTokApi.get_instance(
     custom_verifyFp=verifyFp, use_test_endpoints=True, custom_did='')
 
-#This is a list of dictionaries. Each dictionary contains dict_keys(['id', 'desc', 'createTime', 'video', 'author', 'music', 'challenges', 'stats', 'duetInfo', 'originalItem', 'officalItem', 'textExtra', 'secret', 'forFriend', 'digged', 'itemCommentStatus', 'showNotPass', 'vl1', 'itemMute', 'authorStats', 'privateItem', 'duetEnabled', 'stitchEnabled', 'shareEnabled', 'isAd'])
+
+def tiktok_by_id(id):
+    return api.get_tiktok_by_id(id)
+
+
+# This is a list of dictionaries. Each dictionary contains dict_keys(['id', 'desc', 'createTime', 'video', 'author', 'music', 'challenges', 'stats', 'duetInfo', 'originalItem', 'officalItem', 'textExtra', 'secret', 'forFriend', 'digged', 'itemCommentStatus', 'showNotPass', 'vl1', 'itemMute', 'authorStats', 'privateItem', 'duetEnabled', 'stitchEnabled', 'shareEnabled', 'isAd'])
 def trending():
     return api.trending()
 
 
-def video_by_tiktok(tiktok):
-    video_bytes = api.get_video_by_tiktok(tiktok)
-    with open('tiktok_' + tiktok['id']+ '.mp4', 'wb') as o:
-        o.write(video_bytes)
-
 def video_by_id(id):
+    print('video by id')
     tiktok = tiktok_by_id(id)
-    return video_by_tiktok(tiktok)
+    return video_by_tiktok(tiktok['itemInfo']['itemStruct'])
 
-def tiktok_by_id(id):
-    return api.get_tiktok_by_id(id)
+
+def video_by_tiktok(tiktok):
+    print('video by tiktok')
+    video_bytes = api.get_video_by_tiktok(tiktok)
+    mem = io.BytesIO()
+    mem.write(video_bytes)
+    mem.seek(0)
+    return mem
+
 
 # def get_tiktok_s_v_web_id():
 #     headers = {
@@ -42,6 +48,6 @@ def tiktok_by_id(id):
 
 # if __name__ == "__main__":
 #     params = {}
-#     trending_tiktoks = trending() 
+#     trending_tiktoks = trending()
 #     first_tiktok = trending_tiktoks[0]
 #     get_video_by_id(first_tiktok['id'])
