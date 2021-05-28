94230b9eb634a2c1aaab7e925817cf284013056f
Andrew <andrew.karnis@gmail.com>
2021-05-27 20:16:57 -0400

    add route to get tiktok object

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask.helpers import send_file
-from tokify import trending, video_by_id
+from tokify import tiktok_by_id, trending, video_by_id
 
 app = Flask(__name__)
 
@@ -13,6 +13,11 @@ def get_trending():
 @app.route('/download/<id>')
 def get_video_by_id(id):
     return send_file(video_by_id(id), attachment_filename="tiktok_" + id + ".mp4", as_attachment=True, mimetype='video/mp4')
+
+
+@app.route('/info/<id>')
+def get_tiktok_by_id(id):
+    return jsonify(tiktok_by_id(id))
 
 
 if __name__ == '__main__':
