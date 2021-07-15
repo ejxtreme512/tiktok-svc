1a82e15a95d6dd43b29a5c1ef314ed6f96bce227
Eric <erickarnis@gmail.com>
2021-07-15 19:11:43 -0400

    create database tables

--- sql.py
+++ sql.py
@@ -6,13 +6,17 @@ def execute_queries(queries):
     with closing(sqlite3.connect('tokifydb.db')) as connection:
         with connection:
             for query in queries:
-                result = connection.execut(query)
+                print(query)
+                result = connection.execute(query)
 
 
 def create_query(name, fields):
-    my_query = f'Create table {name} ('
-    for item in fields:
-        my_query = my_query + (f'{item[0]} {item[1]},')
+    my_query = f"Create table {name} ("
+    for i, item in enumerate(fields):
+        new_phrase = (f"{item[0]} {item[1]}")
+        if i != len(fields) - 1:
+            new_phrase += ","
+        my_query = my_query + new_phrase
     my_query = my_query + ")"
     return my_query
 
@@ -20,8 +24,8 @@ def buildDataBase():
     tables = [
         ('USER_FAVORITES', [
             ('user_id', 'INTEGER'),
-            ('list id', 'INTEGER'),
-            ('list name', 'STRING'),
+            ('list_id', 'INTEGER'),
+            ('list_name', 'STRING'),
         ]),
         ('USERS', [
             ('user_id', 'INTEGER'),
@@ -31,8 +35,8 @@ def buildDataBase():
             ('last_name', 'STRING')
         ]),
         ('FAVORITES', [
-            ('user_id', 'INTEGER')
-            ('list_id',	'INTEGER')
+            ('user_id', 'INTEGER'),
+            ('list_id',	'INTEGER'),
             ('video_id', 'INTEGER')
         ])
     ]
