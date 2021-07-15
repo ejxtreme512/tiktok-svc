7bd0a3a80527f15bc67f4a7519d748e80094e366
Eric <erickarnis@gmail.com>
2021-07-15 18:43:43 -0400

    build database

--- sql.py
+++ sql.py
@@ -1,15 +1,12 @@
 import sqlite3
-from sqlite3.dbapi2 import Connection
+from contextlib import closing
 
 
-# create query will be the string passed into create table (ie:Create table users (user_id INTEGER,last_login INTEGER,) )
-def create_table(create_query):
-    print(f'my query is {create_query} ')
-    connection = sqlite3.connect('tokifydb.db')
-    c = connection.cursor()  # create a cursor
-    # c.execute(create_query) #execute table
-    connection.commit()  # Commit our command
-    connection.close()
+def execute_queries(queries):
+    with closing(sqlite3.connect('tokifydb.db')) as connection:
+        with connection:
+            for query in queries:
+                result = connection.execut(query)
 
 
 def create_query(name, fields):
@@ -19,44 +16,31 @@ def create_query(name, fields):
     my_query = my_query + ")"
     return my_query
 
-# def BuildDatabase():
-#     my_tables = ['FAVORITES', 'USER FAVORITES', 'USERS']
-#     my_queries = 
-#     for table in my_tables:
-
-def buildsum():
-    my_var = [[1,2],[10,12],[15,20],[14,17][8,6]]
-    # create a new list with the sum of each item in my_var
-    for list in my_var:
-        my_new_var = 
+def buildDataBase():
+    tables = [
+        ('USER_FAVORITES', [
+            ('user_id', 'INTEGER'),
+            ('list id', 'INTEGER'),
+            ('list name', 'STRING'),
+        ]),
+        ('USERS', [
+            ('user_id', 'INTEGER'),
+            ('last_login', 'INTEGER'),
+            ('email', 'STRING'),
+            ('first_name', 'STRING'),
+            ('last_name', 'STRING')
+        ]),
+        ('FAVORITES', [
+            ('user_id', 'INTEGER')
+            ('list_id',	'INTEGER')
+            ('video_id', 'INTEGER')
+        ])
+    ]
+    queries = []
+    for table_name, table_fields in tables:
+        table_create_query = create_query(table_name, table_fields)
+        queries.append(table_create_query)
+    execute_queries(queries)
 
 if __name__ == "__main__":
-    my_user_name = 'USER_FAVORITES'
-    my_user_fields = [
-        ('user_id', 'INTEGER'),
-        ('list id', 'INTEGER'),
-        ('list name', 'STRING'),
-    ]  
-    # Create table users (user_id INTEGER,last_login INTEGER,)
-    my_query = create_query(my_user_name, my_user_fields)
-    create_table(my_query)
-
-    my_query_name = 'USERS'
-    my_query_fields = [
-        ('user_id', 'INTEGER'),
-        ('last_login', 'INTEGER'),
-        ('email', 'STRING'),
-        ('first_name', 'STRING'),
-        ('last_name', 'STRING')
-    ]
-    my_user_query = create_query(my_query_name, my_query_fields)
-    create_table(my_user_query)
-
-    my_favorites_name = 'FAVORITES'
-    my_favorites_fields = [
-        ('user_id', 'INTEGER')
-        ('list_id',	'INTEGER')
-        ('video_id','INTEGER')
-        ]
-    my_favorite_query = create_query(my_favorites_name, my_favorites_fields)
-    create_table(my_favorite_query)
+    buildDataBase()
