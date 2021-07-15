3b6a9db23cad980b918a44e7e00be97ab9c9c470
Eric <erickarnis@gmail.com>
2021-07-15 19:31:01 -0400

    create method to buildDatabase, add placeholders for interacting with favorites

--- sql.py
+++ sql.py
@@ -1,6 +1,31 @@
 import sqlite3
 from contextlib import closing
 
+USER_FAVORITES = 'USER_FAVORITES'
+USERS = 'USERS'
+FAVORITES = 'FAVORITES'
+
+
+def add_favorite_to_list(user_id, list_id, tiktok_id):
+    execute_query(generate_insert_query(
+        USER_FAVORITES, (user_id, list_id, tiktok_id)))
+
+
+def add_user_to_users(user_id, first_name, last_name):
+    pass
+
+
+def create_favorites_list(user_id, list_name):
+    pass
+
+
+def get_favorites_list_by_user_id():
+    pass
+
+
+def execute_query(query):
+    return execute_queries([query])
+
 
 def execute_queries(queries):
     with closing(sqlite3.connect('tokifydb.db')) as connection:
@@ -10,7 +35,7 @@ def execute_queries(queries):
                 result = connection.execute(query)
 
 
-def create_query(name, fields):
+def generate_create_query(name, fields):
     my_query = f"Create table {name} ("
     for i, item in enumerate(fields):
         new_phrase = (f"{item[0]} {item[1]}")
@@ -20,14 +45,19 @@ def create_query(name, fields):
     my_query = my_query + ")"
     return my_query
 
+
+def generate_insert_query(table_name, values):
+    return f"INSERT INTO {table_name} VALUES {values}"
+
+
 def buildDataBase():
     tables = [
-        ('USER_FAVORITES', [
+        (USER_FAVORITES, [
             ('user_id', 'INTEGER'),
             ('list_id', 'INTEGER'),
             ('list_name', 'STRING'),
         ]),
-        ('USERS', [
+        (USERS, [
             ('user_id', 'INTEGER'),
             ('last_login', 'INTEGER'),
             ('email', 'STRING'),
@@ -37,14 +67,15 @@ def buildDataBase():
         ('FAVORITES', [
             ('user_id', 'INTEGER'),
             ('list_id',	'INTEGER'),
-            ('video_id', 'INTEGER')
+            ('tiktok_id', 'INTEGER')
         ])
     ]
     queries = []
     for table_name, table_fields in tables:
-        table_create_query = create_query(table_name, table_fields)
+        table_create_query = generate_create_query(table_name, table_fields)
         queries.append(table_create_query)
     execute_queries(queries)
+
 
 if __name__ == "__main__":
     buildDataBase()
