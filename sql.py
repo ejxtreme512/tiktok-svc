c46b6cec7381c1c0547b11892decb1fc2e58ade1
Eric <erickarnis@gmail.com>
2021-07-27 21:08:48 -0400

    get list of favorites, get a favorites list

--- sql.py
+++ sql.py
@@ -6,9 +6,9 @@ USERS = 'USERS'
 FAVORITES = 'FAVORITES'
 
 
-def add_favorite_to_list(user_id, list_id, tiktok_id):
+def add_tiktok_to_favorites_list(user_id, list_id, tiktok_id):
     execute_query(generate_insert_query(
-        FAVORITES, (user_id, list_id, tiktok_id)))
+        FAVORITES, (user_id, list_id, tiktok_id)), {})
 
 
 def add_user_to_users(user_id, email, first_name, last_name):
@@ -16,25 +16,47 @@ def add_user_to_users(user_id, email, first_name, last_name):
         USERS, (user_id, None, email, first_name, last_name)))
 
 
-def add_favorites_list(user_id, list_name):
+def add_user_favorites_list(user_id, list_id, list_name):
     execute_query(generate_insert_query(
-        USER_FAVORITES, (user_id, list_name)))
+        USER_FAVORITES, (user_id, list_id, list_name)), {})
 
 
-def get_user_favorites_list():
-    pass
+def get_favorite_items_by_list_id(id):
+    query = '''
+        SELECT UF.user_id, UF.list_id, UF.list_name, F.tiktok_id
+        FROM 
+            USER_FAVORITES AS UF,
+            FAVORITES AS F
+        WHERE UF.list_id = F.list_id
+        AND F.list_id = :id
+    '''
+    parameters = {'id': id}
+    return execute_query(query, parameters)
 
 
-def execute_query(query):
-    return execute_queries([query])
+def get_favorites_list_by_user_id(user_id):
+    query = '''
+        SELECT * 
+        FROM 
+            USER_FAVORITES AS UF
+        WHERE UF.user_id = :userId
+    '''
+    parameters = {'userId': user_id}
+    return execute_query(query, parameters)
+
+
+def execute_query(query, parameters):
+    return execute_queries([[query, parameters]])[0]
 
 
 def execute_queries(queries):
+    results = []
     with closing(sqlite3.connect('tokifydb.db')) as connection:
         with connection:
-            for query in queries:
-                print(query)
-                result = connection.execute(query)
+            for query, parameters in queries:
+                result = connection.execute(query, parameters)
+                results.append(result.fetchall())
+    return results
 
 
 def generate_create_query(name, fields):
@@ -75,9 +97,12 @@ def buildDataBase():
     queries = []
     for table_name, table_fields in tables:
         table_create_query = generate_create_query(table_name, table_fields)
-        queries.append(table_create_query)
+        queries.append([table_create_query, {}])
     execute_queries(queries)
 
 
 if __name__ == "__main__":
-    buildDataBase()
+    # add_user_favorites_list(2, 30, 'testABC')
+    # add_tiktok_to_favorites_list(1,100, 3420)
+    print(get_favorite_items_by_list_id(30))
+    print(get_favorites_list_by_user_id(2))
